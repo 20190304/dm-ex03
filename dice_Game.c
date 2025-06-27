ab58729ba973bd08f8c81480dd2ec422c9d1db72
20190304 <yuuna_meguro@icloud.com>
2025-06-27 11:53:01 +0900

    Dice game: initial revision

--- dice_Game.c
+++ dice_Game.c
@@ -1,1 +1,20 @@
-aaaaaa
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int die1, die2, total;
+  
+  srand(time(NULL));
+
+die1 = (rand() % 6) + 1;
+die2 = (rand() % 6) + 1;
+total = die1 + die2;
+
+printf("Rolling dice...\n");  
+printf("Die1: %d\n", die1);
+printf("Die2: %d\n", die2);  
+printf("Toatal value: %d\n", total);
+
+return 0;
+}
