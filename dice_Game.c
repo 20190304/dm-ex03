bd0e5c7378231455348c6a891745dc82ae48f783
20190304 <yuuna_meguro@icloud.com>
2025-06-27 12:03:32 +0900

    Greetings added

--- dice_Game.c
+++ dice_Game.c
@@ -1,1 +1,26 @@
-aaaaaa
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  char name[50];
+  int die1, die2, total;
+
+  printf("What is your name?\n> ");
+  scanf("%s", name);
+
+  printf("Hello, %s!\n", name);
+  
+  srand(time(NULL)); 
+
+die1 = (rand() % 6) + 1;
+die2 = (rand() % 6) + 1;
+total = die1 + die2;
+
+printf("Rolling dice...\n");  
+printf("Die1: %d\n", die1);
+printf("Die2: %d\n", die2);  
+printf("Toatal value: %d\n", total);
+
+return 0;
+}
