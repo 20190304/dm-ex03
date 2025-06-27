68b44b7ec80f9d446f6ff4846266615484449a18
20190304 <yuuna_meguro@icloud.com>
2025-06-27 15:01:41 +0900

    Won/lost message modified

--- dice_Game.c
+++ dice_Game.c
@@ -25,9 +25,9 @@ printf("Die2: %d\n", die2);
 printf("Toatal value: %d\n", total);
 
 if (total > 7) {
-  printf("You won!\n");
+  printf("%s won!\n", name);
 } else {
-  printf("You lost!\n");
+  printf("%s lost!\n", name);
 }
  
 return 0;
